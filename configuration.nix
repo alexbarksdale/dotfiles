f69db2a070295729a9143173cc517194bf37cdfd
Alex Barksdale <alx.barksdale@gmail.com>
2026-07-05 22:26:39 -0400

    more mac config

--- configuration.nix
+++ configuration.nix
@@ -12,4 +12,17 @@
     home = "/Users/alexbarksdale/";
   };
   system.stateVersion = 6;
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      KeyRepeat = 2;          # fast key repeat
+      InitialKeyRepeat = 15;  # short delay before repeat
+      _HIHideMenuBar = true;  # auto-hide the menu bar
+      AppleShowAllExtensions = true;
+    };
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+    finder.CreateDesktop = true;          # clean desktop
+    trackpad.Clicking = true;              # tap to click
+  };
 }
